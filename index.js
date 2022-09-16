bae42ac852c804ebf4c53ef1144b22c771aeb8c1
Darko V <darko1290@gmail.com>
2022-09-17 00:10:49 +0200

    Update test.kv and add text token to comments

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ function parseKV (data) {
         debug('found stuff outside of quotes', line);
         // throw new Error('Unexpected token "' + token + '" on line ' + entry.line);
         console.log('Warning: Unexpected text on line ' + entry.line + ': "' + token + '"');
+        comment += token;
       }
     });
     if (isInQuotes) {
